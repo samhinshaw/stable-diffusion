563661d60bec18ee9ca606f3e4a0b59959c70a8a
Sam Hinshaw <samuel.hinshaw@gmail.com>
2022-09-03 06:42:18 -0700

    generate multiple images at once

--- generate.py
+++ generate.py
@@ -1,11 +1,24 @@
+from PIL import Image
 from pathlib import Path
 from diffusers import StableDiffusionPipeline
 from os import path
-from output import save
+from output import output
+import torch
 
 # what do we want a picture of?
-PROMPT = "a skyscraper designed by frank lloyd wright"
-SAVE_DIR = "."
+# PROMPT = "photograph portrait osama bin laden by platon"
+prompts = [
+    "portrait of Bugs Bunny by Leonardo da Vinci",
+    "portrait of Bugs Bunny by Monet",
+    "portrait of Bugs Bunny by Rembrant",
+    "portrait of Bugs Bunny by Frida Kahlo",
+    "portrait of Bugs Bunny by Picasso",
+    "portrait of Bugs Bunny by Vermeer",
+    "portrait of Bugs Bunny by Cezanne",
+    "portrait of Bugs Bunny by Andy Warhol",
+    "portrait of Bugs Bunny by Caravaggio",
+]
+SAVE_DIR = "results"
 
 # get the huggingface token
 def get_token():
@@ -15,16 +28,48 @@ def get_token():
     return token
 
 
-# set up pipe
-pipe = StableDiffusionPipeline.from_pretrained(
-    "CompVis/stable-diffusion-v1-4", use_auth_token=get_token()
-)
+def image_grid(imgs, rows, cols):
+    assert len(imgs) == rows * cols
 
-# use the GPU
-pipe.to("cuda")
+    w, h = imgs[0].size
+    grid = Image.new("RGB", size=(cols * w, rows * h))
+    grid_w, grid_h = grid.size
 
-# make the image!
-image = pipe(PROMPT)["sample"][0]
+    for i, img in enumerate(imgs):
+        grid.paste(img, box=(i % cols * w, i // cols * h))
+    return grid
 
-# save it and send it!
-save(image, SAVE_DIR, PROMPT)
+
+def generate_grid(prompts: list[str] | str) -> Image:
+    all_images = []
+    for prompt in prompts:
+        with torch.autocast("cuda"):
+            image = [generate(prompt)]
+        all_images.extend(image)
+
+    grid = image_grid(all_images, rows=3, cols=3)
+    return grid
+
+
+def generate(prompt: str) -> Image:
+    # set up pipe
+    pipe = StableDiffusionPipeline.from_pretrained(
+        "CompVis/stable-diffusion-v1-4",
+        use_auth_token=get_token(),
+    )
+
+    pipe.to("cuda")
+
+    # generator = torch.Generator("cuda").manual_seed(1026)
+    image = pipe(
+        prompt,
+        height=512,
+        width=512,
+        guidance_scale=6,  # generator=generator
+    )["sample"][0]
+    return image
+
+
+image = generate_grid(prompts)
+# save it to disk and upload the results!
+output(image, SAVE_DIR, description="\n ".join(prompts))
--- output.py
+++ output.py
@@ -1,29 +1,66 @@
 import re
-from PIL import Image
-from io import BufferedReader
 import requests
-import os
-import path
+import flickrapi
+from PIL import Image
+from io import BufferedReader, BytesIO
+from os import environ, path
 
-USER_KEY = os.environ["PUSHOVER_USER_KEY"]
-APP_TOKEN = os.environ["PUSHOVER_APP_TOKEN"]
+PUSHOVER_USER_KEY = environ["PUSHOVER_USER_KEY"]
+PUSHOVER_APP_TOKEN = environ["PUSHOVER_APP_TOKEN"]
+
+FLICKR_APP_KEY = environ["FLICKR_APP_KEY"]
+FLICKR_APP_SECRET = environ["FLICKR_APP_SECRET"]
+
+
+def uploadToFlickr(filepath: str, title: str, description: str):
+    api = flickrapi.FlickrAPI(FLICKR_APP_KEY, FLICKR_APP_SECRET)
+
+    if not api.token_valid():
+        # OOB: out of band
+        api.get_request_token(oauth_callback="oob")
+
+        verifier = str(
+            input(
+                f"Get verifier code from {api.auth_url(perms='write')} and enter it here.\n: "
+            )
+        )
+
+        # Get access token and store it as ${HOME}/.flickr/oauth-tokens.sqlite.
+        # If you want to remove the cache, call api.token_cache.forget().
+        api.get_access_token(verifier)
+        # res is instance of xml.etree.ElementTree.Element.
+    # This element has something like "<rsp><photoid>1234</photoid></rsp>".
+    res = api.upload(
+        filename=filepath,
+        title=title,
+        description=description,
+        is_private=True,
+    )
+    print(res)
+
 
 # Send image data to pushover with a message string
 def sendToPushover(imageBytes: BufferedReader, filename: str, message: str):
     r = requests.post(
         "https://api.pushover.net/1/messages.json",
-        data={"token": APP_TOKEN, "user": USER_KEY, "message": message},
+        data={
+            "token": PUSHOVER_APP_TOKEN,
+            "user": PUSHOVER_USER_KEY,
+            "message": message,
+        },
         files={"attachment": (filename, imageBytes, "image/png")},
-        # files={"attachment": ("image.jpg", open("your_image.jpg", "rb"), "image/jpeg")},
     )
-    return r.text
+    print(r.text)
 
 
-def save(image: Image, dir: str, description: str) -> None:
-    filename = re.sub(r"[^a-zA-Z]+", "-", description) + ".png"
+def output(image: Image, dir: str, description: str) -> None:
+    trimmedDescription = description[0:20]
+    filename = re.sub(r"[^a-zA-Z]+", "-", trimmedDescription) + ".png"
     filepath = path.join(dir, filename)
     image.save(filepath)
-    # it's weirdly difficult to turn this image into byte data.
-    # one of the easiest ways is just to re-read the file from the filesystem
-    imageBytes = open(filepath, "rb")
-    sendToPushover(imageBytes, filename, description)
+
+    uploadToFlickr(
+        filepath,
+        title=trimmedDescription,
+        description=description,
+    )
